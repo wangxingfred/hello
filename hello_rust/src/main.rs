659803ec97013ce8387e2183dea8b78497d81668
fred <wangxingfred@gmail.com>
2020-12-15 08:05:25 +0800

    hello rust

--- hello_rust/src/main.rs
+++ hello_rust/src/main.rs
@@ -1,40 +1,64 @@
-use structopt::StructOpt;
-use failure::ResultExt;
-use exitfailure::ExitFailure;
-use log::{info,warn,debug};
+fn main() {
+    let tuple = (1, 2.2, b'a');
 
-/// Search for a pattern in a file and display the lines that contain it.
-#[derive(StructOpt)]
-struct Cli {
-    /// The pattern to look for
-    pattern: String,
-    /// The path to the file to read
-    #[structopt(parse(from_os_str))]
-    path: std::path::PathBuf,
+    println!("tuple = ({}, {}, {})", tuple.0, tuple.1, tuple.2);
+
+    let array1: [u32; 5] = [1, 2, 3, 4, 5];
+    let array2: [u32; 5] = [9; 5];
+
+    let index = 6;
+
+    println!("element at {} : {}", index, array1[index]);
 }
 
-fn main() -> Result<(), ExitFailure>{
-    let args = Cli::from_args();
-    let content = std::fs::read_to_string(&args.path).
-        with_context(|_|format!("could not read file '{}'", args.path.display()))?;
-    find_matches(&content, &args.pattern, &mut std::io::stdout());
-//    env_logger:init();
-//    info!("starting up");
-//    warn!("oops, nothing implemented!");
-    Ok(())
-}
+// use std::io;
+// use rand::Rng;
+// use std::cmp::Ordering;
+//
+// fn main() {
+//     println!("Guess the number");
+//
+//     let secret_number = rand::thread_rng().gen_range(1, 101);
+//
+//     loop {
+//         println!("Please input your guess:");
+//
+//         let mut guess = String::new();
+//
+//         io::stdin().read_line(&mut guess).expect("Failed to read line");
+//
+//         let guess: u32 = match guess.trim().parse() {
+//             Ok(num) => num,
+//             Err(_) => continue
+//         };
+//
+//         println!("You guessed:{0}", guess);
+//
+//         match guess.cmp(&secret_number) {
+//             Ordering::Less => println!("Too small!"),
+//             Ordering::Greater => println!("Too big!"),
+//             Ordering::Equal => {
+//                 println!("You win!");
+//                 break;
+//             }
+//         }
+//     }
+//
+// }
 
-pub fn find_matches(content: &str, pattern: &str, mut writer: &impl std::io::Write) {
-    for line in content.lines() {
-        if line.contains(pattern) {
-            writeln!(writer, "{}", line);
-        }
-    }
-}
 
-#[test]
-fn find_a_match() {
-    let mut result = Vec::new();
-    find_matches("lorem ipsum\ndolor sit amet", "lorem", &result);
-    assert_eq!(result, b"lorem ipsum\n");
-}
+// use ferris_says::say;
+// use std::io::{stdout, BufWriter};
+//
+// fn main() {
+//     let stdout = stdout();
+//     let message = String::from("Hello fellow Rustaceans!");
+//     let width = message.chars().count();
+//
+//     let mut writer = BufWriter::new(stdout.lock());
+//     say(message.as_bytes(), width, &mut writer).unwrap();
+//
+//     println!("Hello, world!");
+// }
+
+
