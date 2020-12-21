010030a6e2e2b8e9726225dd02e98716b0301097
fred <wangxingfred@gmail.com>
2020-12-21 10:29:50 +0800

    hello rust book
    finish '11.1. How to Write Tests'

--- hello_rust/src/lib.rs
+++ hello_rust/src/lib.rs
@@ -1,49 +1,81 @@
-mod front_of_house;
+pub fn greeting(name: &str) -> String {
+    // format!("Hello {}!", name)
+    String::from("Hello!")
+}
 
-mod back_of_house {
-    pub struct Breakfast {
-        pub appetizer: Appetizer,
-        pub toast: String,
-        seasonal_fruit: String
-    }
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-    impl Breakfast {
-        pub fn summer(toast: &str) -> Breakfast {
-            Breakfast {
-                appetizer: Appetizer::Soup,
-                toast: String::from(toast),
-                seasonal_fruit: String::from("banana")
-            }
-        }
-
-        pub fn take(&self) {
-            println!("take breakfast : appetizer = {:?}, toast = {}, fruit = {}",
-                     self.appetizer, self.toast, self.seasonal_fruit)
-        }
-    }
-
-    #[derive(Debug)]
-    pub enum Appetizer {
-        Soup,
-        Salad,
+    #[test]
+    fn greeting_contains_name() {
+        let result = greeting("Carol");
+        assert!(result.contains("Carol"),
+        "Greeting did not contain name, value was `{}`", result);
     }
 }
 
-use front_of_house::hosting;
-use back_of_house::{Breakfast, Appetizer};
 
-// use std::collections::LinkedList;
+// #[cfg(test)]
+// mod tests {
+//     #[test]
+//     fn exploration() {
+//         assert_eq!(2 + 2, 4);
+//     }
+//
+//     #[test]
+//     fn another() {
+//         panic!("Make this test fail!");
+//     }
+// }
 
-// use std::fmt::Result;
-// use std::io::Result as IoResult;
 
-pub fn eat_at_restaurant() {
-    hosting::seat_at_table();
-
-    let mut meal = Breakfast::summer("Rye");
-
-    meal.appetizer = Appetizer::Salad;
-    meal.toast = String::from("white");
-
-    meal.take()
-}
+// mod front_of_house;
+//
+// mod back_of_house {
+//     pub struct Breakfast {
+//         pub appetizer: Appetizer,
+//         pub toast: String,
+//         seasonal_fruit: String
+//     }
+//
+//     impl Breakfast {
+//         pub fn summer(toast: &str) -> Breakfast {
+//             Breakfast {
+//                 appetizer: Appetizer::Soup,
+//                 toast: String::from(toast),
+//                 seasonal_fruit: String::from("banana")
+//             }
+//         }
+//
+//         pub fn take(&self) {
+//             println!("take breakfast : appetizer = {:?}, toast = {}, fruit = {}",
+//                      self.appetizer, self.toast, self.seasonal_fruit)
+//         }
+//     }
+//
+//     #[derive(Debug)]
+//     pub enum Appetizer {
+//         Soup,
+//         Salad,
+//     }
+// }
+//
+// use front_of_house::hosting;
+// use back_of_house::{Breakfast, Appetizer};
+//
+// // use std::collections::LinkedList;
+//
+// // use std::fmt::Result;
+// // use std::io::Result as IoResult;
+//
+// pub fn eat_at_restaurant() {
+//     hosting::seat_at_table();
+//
+//     let mut meal = Breakfast::summer("Rye");
+//
+//     meal.appetizer = Appetizer::Salad;
+//     meal.toast = String::from("white");
+//
+//     meal.take()
+// }
