8042da5f18c90201d1e35969687b8ef4809beaee
fred <wangxingfred@gmail.com>
2020-12-28 13:07:34 +0800

    hello rust book
    finish 16.4

--- hello_rust/hi/src/main.rs
+++ hello_rust/hi/src/main.rs
@@ -1,24 +1,68 @@
-use std::rc::Rc;
-
-enum List {
-    Cons(i32, Rc<List>),
-    Nil
-}
+use std::thread;
+use std::time::Duration;
+use std::sync::mpsc;
+use std::thread::JoinHandle;
 
 fn main () {
-    let a = Rc::new(List::Cons(5, Rc::new(List::Cons(6, Rc::new(List::Nil)))));
-    println!("strong count : {}", Rc::strong_count(&a));
+    let v = vec![1,2,3];
+    let v2 = vec![4,5,6];
 
-    let b = List::Cons(3, Rc::clone(&a));
-    println!("strong count : {}", Rc::strong_count(&a));
+    let (tx, rx) = mpsc::channel();
 
-    {
-        let c = List::Cons(4, Rc::clone(&a));
-        println!("strong count : {}", Rc::strong_count(&a));
+    let mut handle= Option::None;
+    // for _i in 1..2 {
+    //     handle = Option::Some(thread::spawn(move || {
+    //         println!("spawned thread, v : {:?}", v);
+    //         for i in 1..10 {
+    //             tx.send(i).unwrap();
+    //             // println!("spawned thread, hi number {}", i);
+    //             thread::sleep(Duration::from_millis(1));
+    //         }
+    //     }));
+    // }
+
+    handle = Option::Some(thread::spawn(move || {
+        println!("spawned thread, v : {:?}", v);
+        for i in 1..10 {
+            tx.send(i).unwrap();
+            // println!("spawned thread, hi number {}", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    }));
+
+    println!("main thread, v2 : {:?}", v2);
+    for val in rx {
+        println!("main thread, received : {}", val);
     }
+    // for i in 1..5 {
+    //     println!("main thread, hi number {}", i);
+    //     thread::sleep(Duration::from_millis(1));
+    // }
 
-    println!("strong count : {}", Rc::strong_count(&a));
+    handle.unwrap().join().unwrap();
 }
+
+// use std::rc::Rc;
+//
+// enum List {
+//     Cons(i32, Rc<List>),
+//     Nil
+// }
+//
+// fn main () {
+//     let a = Rc::new(List::Cons(5, Rc::new(List::Cons(6, Rc::new(List::Nil)))));
+//     println!("strong count : {}", Rc::strong_count(&a));
+//
+//     let b = List::Cons(3, Rc::clone(&a));
+//     println!("strong count : {}", Rc::strong_count(&a));
+//
+//     {
+//         let c = List::Cons(4, Rc::clone(&a));
+//         println!("strong count : {}", Rc::strong_count(&a));
+//     }
+//
+//     println!("strong count : {}", Rc::strong_count(&a));
+// }
 
 
 // struct MySmartPointer {
