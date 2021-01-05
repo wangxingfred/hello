6a6dda9a2b43de01dd577453044a9434bca716fe
fred <wangxingfred@gmail.com>
2021-01-05 13:19:46 +0800

    hello rust book
    complete

--- hello_rust/hey/src/main.rs
+++ hello_rust/hey/src/main.rs
@@ -1,16 +1,22 @@
 use std::net::{TcpListener, TcpStream};
 use std::io::{Read, Write};
+use std::{thread, time};
 use std::fs;
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
 
-    let get = String::from_utf8_lossy(&buffer);
+    // let get = String::from_utf8_lossy(&buffer);
+    // println!("get : {}", get);
 
-    println!("get : {}", get);
+    let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
-    let (status, filename) = if get.starts_with("GET / HTTP/1.1") {
+    let (status, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "hey.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(time::Duration::from_secs(5));
         ("HTTP/1.1 200 OK", "hey.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND", "404.html")
