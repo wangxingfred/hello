a00dc9f9090abe72d3c8379c98709a12ed538be1
fred <wangxingfred@gmail.com>
2022-03-27 17:48:19 +0800

    hello erlang
    array & map test with otp24

--- hello_erlang/src/map_test.erl
+++ hello_erlang/src/map_test.erl
@@ -1,31 +1,35 @@
 %%%-------------------------------------------------------------------
 %%% @author fred
-%%% @copyright (C) 2021, <Woobest>
+%%% @copyright (C) 2022, <Woobest>
 %%% @doc
-%%%     Compiler test cases
+%%%     TODO 模块描述
 %%% @end
-%%% Created : 29. Jun 2021 5:12 PM
+%%% Created : 19. 3月 2022 17:34
 %%%-------------------------------------------------------------------
--module(compiler_test).
+-module(map_test).
 
 -copyright({jzyx, 'www.jzyx.com'}).
 -author({fred, 'wangxingfred@gmail.com'}).
 -vsn(1).
 
+%% c(map_test, ['P']).
+%% c(map_test, ['E']).
+%% c(map_test, ['S']).
+
 %%%===============================INCLUDE================================
 
 %%%================================DEFINE================================
 %% 不需要给其它模块用的宏定义可以写到这里，避免放到头文件中修改后增加编译时间
+-define(SMALL_MAP_LIMIT, 32).
 
 %%%================================EXPORT================================
--export([a/0, b/0]).
+-compile([export_all, nowarn_export_all]).
 
-a() ->
-    receive
-        _ -> ok
-    after 5000 ->
-        {error, timeout}
-    end.
+flatmap() ->
+    maps:from_keys([a,b,c], 1).
 
-b() ->
-    erlang:send_after(5000, self(), timeout).
+flatmap_keys(Map) when is_map(Map) ->
+    erts_internal:map_to_tuple_keys(Map).
+
+is_flatmap(Map) ->
+    erts_internal:term_type(Map) =:= flatmap.
