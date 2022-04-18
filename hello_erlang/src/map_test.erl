0f3ddd3bd43f1756d25e74dcb8c89da9c24ec68e
fred <wangxingfred@gmail.com>
2022-04-18 20:32:41 +0800

    hello erlang

--- hello_erlang/src/map_test.erl
+++ hello_erlang/src/map_test.erl
@@ -22,6 +22,37 @@
 %% 不需要给其它模块用的宏定义可以写到这里，避免放到头文件中修改后增加编译时间
 -define(SMALL_MAP_LIMIT, 32).
 
+-define(MAP_1, #{a => 1, b => 2}).
+-define(MAP_2, ?MAP_1#{c => 3}).
+
+-define(MAP_3, #{
+    node => node(), pid => self(),
+    mfa => {?MODULE, ?FUNCTION_NAME, ?FUNCTION_ARITY},
+    time => now_millisecond()
+}).
+-define(MAP_4, ?MAP_3#{line => ?LINE}).
+
+-define(MAP_5,
+    begin
+        Node = node(),
+        Pid = self(),
+        MFA = {?MODULE, ?FUNCTION_NAME, ?FUNCTION_ARITY},
+        Time = now_millisecond(),
+        #{node => Node, pid => Pid ,mfa => MFA, time => Time}
+    end).
+-define(MAP_6,
+    begin
+        Line = ?LINE,
+        ?MAP_5#{line => Line}
+    end).
+
+-define(MAP_7, #{
+    node => node, pid => pid,
+    mfa => {?MODULE, ?FUNCTION_NAME, ?FUNCTION_ARITY}, time => 0}#{
+    node := node(), pid := self(), time := now_millisecond()
+}).
+-define(MAP_8, ?MAP_7#{line => ?LINE}).
+
 %%%================================EXPORT================================
 -compile([export_all, nowarn_export_all]).
 
@@ -33,3 +64,27 @@ flatmap_keys(Map) when is_map(Map) ->
 
 is_flatmap(Map) ->
     erts_internal:term_type(Map) =:= flatmap.
+
+
+
+get_value_1(Map, Key) ->
+    #{Key := Value} = Map,
+    Value.
+
+get_value_2(Map, Key) ->
+    maps:get(Key, Map).
+
+get_value_3(Map, Key) ->
+    map_get(Key, Map).
+
+map1() -> ?MAP_1.
+map2() -> ?MAP_2.
+map3() -> ?MAP_3.
+map4() -> ?MAP_4.
+map5() -> ?MAP_5.
+map6() -> ?MAP_6.
+map7() -> ?MAP_7.
+map8() -> ?MAP_8.
+
+now_millisecond() ->
+    os:system_time(millisecond).
